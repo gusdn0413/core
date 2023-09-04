f3a653d8d96c607f5f4963e377a916cece1d383e
gusdn0413 <141606385+gusdn0413@users.noreply.github.com>
2023-09-05 04:58:16 +0900

    주문과 할인 도메인 실행과 테스트

--- src/test/java/hello/core/order/OrderServiceTest.java
+++ src/test/java/hello/core/order/OrderServiceTest.java
@@ -1,27 +1,13 @@
 package hello.core.order;
 
-import hello.core.AppConfig;
-import hello.core.discount.DiscountPolicy;
-import hello.core.member.Grade;
-import hello.core.member.Member;
-import hello.core.member.MemberRepository;
-import hello.core.member.MemberService;
+import hello.core.member.*;
 import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 
 public class OrderServiceTest {
 
-    MemberService memberService;
-    OrderService orderService;
-
-    @BeforeEach
-    public void beforeEach() {
-        AppConfig appConfig = new AppConfig();
-        memberService = appConfig.memberService();
-        orderService = appConfig.orderService();
-    }
+    MemberService memberService = new MemberServiceImpl();
+    OrderService orderService = new OrderServiceImpl();
 
     @Test
     void createOrder() {
