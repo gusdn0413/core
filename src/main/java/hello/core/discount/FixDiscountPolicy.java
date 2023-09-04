f62db2e22b282a5b6d0c3bb848ed236d88bfb523
gusdn0413 <141606385+gusdn0413@users.noreply.github.com>
2023-09-05 04:51:59 +0900

    주문과 할인 도메인 개발

--- src/main/java/hello/core/discount/FixDiscountPolicy.java
+++ src/main/java/hello/core/discount/FixDiscountPolicy.java
@@ -5,11 +5,9 @@ import hello.core.member.Member;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
-@Component
 public class FixDiscountPolicy implements DiscountPolicy {
 
     private int discountFixAmount = 1000;
-
     @Override
     public int discount(Member member, int price) {
         if (member.getGrade() == Grade.VIP) {
