d08dceff8b0c18d60f0f87f39748cb9af0cd23c1
gusdn0413 <141606385+gusdn0413@users.noreply.github.com>
2023-09-05 04:31:27 +0900

    회원 도메인 실행 테스트

--- src/test/java/hello/core/member/MemberServiceTest.java
+++ src/test/java/hello/core/member/MemberServiceTest.java
@@ -7,12 +7,8 @@ import org.junit.jupiter.api.Test;
 
 public class MemberServiceTest {
 
-    MemberService memberService;
-    @BeforeEach
-    public void beforeEach() {
-        AppConfig appConfig = new AppConfig();
-        memberService = appConfig.memberService();
-    }
+    MemberService memberService = new MemberServiceImpl();
+
     @Test
     void join() {
         //given
