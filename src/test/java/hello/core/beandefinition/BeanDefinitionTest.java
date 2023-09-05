d1771261f4d53fece0bd33707235f8e085207aaa
gusdn0413 <141606385+gusdn0413@users.noreply.github.com>
2023-09-06 06:56:05 +0900

    스프링 빈 설정 메타 정보 - BeanDefinition

--- src/test/java/hello/core/beandefinition/BeanDefinitionTest.java
+++ src/test/java/hello/core/beandefinition/BeanDefinitionTest.java
@@ -11,7 +11,7 @@ public class BeanDefinitionTest {
         AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
 //        GenericXmlApplicationContext ac = new GenericXmlApplicationContext("appConfig.xml");
     @Test
-    @DisplayName("빈 설정 메타 정보 확인")
+    @DisplayName("빈 설정 메타정보 확인")
     void findApplicationBean() {
         String[] beanDefinitionNames = ac.getBeanDefinitionNames();
         for (String beanDefinitionName : beanDefinitionNames) {
