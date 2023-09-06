f5399f18ed7f6f3ebabd391cf19cc58585e9a2cd
gusdn0413 <141606385+gusdn0413@users.noreply.github.com>
2023-09-07 05:22:42 +0900

    싱글톤 패턴

--- src/test/java/hello/core/singleton/SingletonTest.java
+++ src/test/java/hello/core/singleton/SingletonTest.java
@@ -40,27 +40,7 @@ public class SingletonTest {
         System.out.println("singletonService2 = " + singletonService2);
 
         assertThat(singletonService1).isSameAs(singletonService2);
-    }
 
-    @Test
-    @DisplayName("스프링 컨테이너와 싱글톤")
-    void springContainer() {
 
-//        AppConfig appConfig = new AppConfig();
-        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
-        // 1. 조회 : 호출할 때 마다 객체를 생성
-//        MemberService memberService1 = appConfig.memberService();
-        MemberService memberService1 = ac.getBean("memberService",MemberService.class);
-
-        // 2. 조회 : 호출할 때 마다 객체를 생성
-//        MemberService memberService2 = appConfig.memberService();
-        MemberService memberService2 = ac.getBean("memberService", MemberService.class);
-
-        // 참조 값이 다른 것을 확인
-        System.out.println("memberService1 = " + memberService1);
-        System.out.println("memberService2 = " + memberService2);
-
-        // memberService1 != memberService2
-        assertThat(memberService1).isSameAs(memberService2);
     }
 }
